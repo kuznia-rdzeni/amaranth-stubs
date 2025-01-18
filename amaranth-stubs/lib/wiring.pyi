e25a7fa11d4a0d66ed18190f31b60914f222e74c
Piotr Wegrzyn <piotro@piotro.eu>
2025-01-18 16:57:41 +0100

    lib.wiring: use `AbstractSignature` in `Flow` and `Member`

--- amaranth-stubs/lib/wiring.pyi
+++ amaranth-stubs/lib/wiring.pyi
@@ -33,7 +33,7 @@ class Flow(enum.Enum):
         """
         ...
     
-    def __call__(self, description: Signature | ShapeLike, *, reset=...) -> Member:
+    def __call__(self, description: AbstractSignature | ShapeLike, *, reset=...) -> Member:
         """Create a :class:`Member` with this data flow and the provided description and
         reset value.
 
@@ -56,7 +56,7 @@ Out = Flow.Out
 In = Flow.In
 
 @final
-class Member:
+class Member(Generic[_T_Signature]):
     """Description of a signature member.
 
     This class is a discriminated union: its instances describe either a `port member` or
@@ -75,7 +75,7 @@ class Member:
     Although instances can be created directly, most often they will be created through
     :data:`In` and :data:`Out`, e.g. :pc:`In(unsigned(1))` or :pc:`Out(stream.Signature(RGBPixel))`.
     """
-    def __init__(self, flow: Flow, description: Signature | ShapeLike, *, reset=..., _dimensions=...) -> None:
+    def __init__(self, flow: Flow, description: _T_Signature | ShapeLike, *, reset=..., _dimensions=...) -> None:
         ...
     
     def flip(self) -> Member:
@@ -178,7 +178,7 @@ class Member:
         ...
     
     @property
-    def signature(self) -> Signature:
+    def signature(self) -> _T_Signature:
         """Signature of a signature member.
 
         Returns
